1c0fec34e16ff3c046818c6f5d14440b83fe4bdc
hayden-prewett <haydenprewett@outlook.com>
2026-03-06 22:14:21 +0000

    Added delete login function

--- PasswordManager/LoginInfo.cs
+++ PasswordManager/LoginInfo.cs
@@ -47,6 +47,36 @@ namespace PasswordManager
         private void btnDeleteLogin_Click(object sender, EventArgs e)
         {
             // Ask for confirmation, then delete the record from the data base
+            var result = MessageBox.Show("Are you sure you want to delete this login?", "Confirm", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                // Delete the record
+                string filePath = @"..\..\..\testDB.csv";
+                List<string> lines = File.ReadLines(filePath).ToList(); // read the lines from the csv
+                // Loop through the data until the record is found. This is also used in ChangePassword.cs and
+                // should be put into one function at some point.
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    string line = lines[i]; // the current line
+                    string[] parts = line.Split(","); // split the line into its values
+
+                    // Check if the line is correct based on username and password
+                    if (parts[0].Trim() == _website && parts[1].Trim() == _username)
+                    {
+                        // Record found! Delete it.
+                        lines.Remove(line);
+                        MessageBox.Show("Login deleted", "Success");
+                        break;
+                    }
+                }
+
+                // Write all the lines back to the CSV, including updated one
+                File.WriteAllLines(filePath, lines);
+            }
+            else
+            {
+                this.Close();
+            }
         }
     }
 }
